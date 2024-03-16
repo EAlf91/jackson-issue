5fa6470ad4789b4f4225bf27e3d3d1068ea5db9c
Eric Brandt <ericbrandt91@gmail.com>
2024-03-16 19:43:37 +0100

    reproduced issue

---
+++ src/test/java/BigDecimalTest.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.io.NumberInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BigDecimalTest {
+
+    ObjectMapper mapper = new ObjectMapper();
+    BigDecimal expected = BigDecimal.valueOf(0.05);
+
+    @Test
+    public void shouldDeserializeBigDecimalFromJacksonCore() throws JsonProcessingException {
+
+        BigDecimal actualFromJacksonCore = NumberInput.parseBigDecimal(".05", true);
+
+        assertEquals(expected, actualFromJacksonCore);
+    }
+
+    @Test
+    public void shouldDeserializeBigDecimalFromJacksonDataBind() throws JsonProcessingException {
+
+        BigDecimal actualFromJacksonDataBind = mapper.readValue("""
+                                                                        {"value":".05"}
+                                                                        """, TestDTO.class).getValue();
+
+        assertEquals(expected, actualFromJacksonDataBind);
+    }
+
+}
---
+++ src/test/java/TestDTO.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class TestDTO {
+
+    private BigDecimal value;
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
